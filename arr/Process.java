22f849a86ef58bed59e18ef1dfb8f562d01ee105
Juan Pablo Vargas <jpvs1609@gmail.com>
2025-07-20 13:54:38 -0400

    Feat: añadiendo los metodos faltantes

--- arr/Process.java
+++ arr/Process.java
@@ -17,29 +17,27 @@ public class Process {
         }
     }
 
-    public static void iniDouble(double[] average) {
-        if (average != null) {
-            for (int i = 0; i < average.length; i++) {
-
-                average[i] = 0;
-
+    public static void iniDouble(double [] doubles){
+        if (doubles != null){
+            for (int i = 0; i < doubles.length; i++) {
+                doubles [i] = 0;
             }
         }
     }
 
-    public static void initStudentNames(String[][] array) {
-        if (array != null) {
-            for (int i = 0; i < array.length; i++) {
-                for (int j = 0; j < array[0].length; j++) {
-                    array[i][j] = "";
+    public static void iniAverage(double[][] average) {
+        if (average != null) {
+            for (int i = 0; i < average.length; i++) {
+                for (int j = 0; j < average[i].length; j++) {
+                    average[i][j] = 0;
                 }
             }
         }
     }
 
-    public static void iniString(String[] array) {
+    public static void initNames(String[] array) {
         if (array != null) {
-            for(int i = 0; i < array.length; i++){
+            for (int i = 0; i < array.length; i++) {
                 array[i] = "";
             }
         }
@@ -48,19 +46,16 @@ public class Process {
 
     public static void fillArrays(double[][][] customers, String[] customersnames) {
     Scanner scanner = new Scanner(System.in);
-    String text, name = "";
-        if (customers != null && customersnames != null) {
+    String texts = "";
+        if (customers.length != 0 && customersnames.length != 0) {
             for (int x = 0; x < customers.length; x++) {
-                text = "Ingrese el nombre del propietario de la casa [" + x + "]: ";
-                System.out.print(text);
-                name = scanner.nextLine();
-                customersnames[x] = Validations.valSubName(name,text);
+                texts = "Ingrese el nombre del propietario de la casa [" + (x+1) + "]: ";
+                System.out.print(texts);
+                customersnames[x] = Validations.valSubName(scanner.nextLine(),texts);
                 for (int y = 0; y < customers[x].length; y++) {
-                    text = "Mes: " + (y + 1);
-                    System.out.println(text);
+                    System.out.println("Mes: " + (y + 1));
                     for (int z = 0; z < customers[x][y].length; z++) {
-                        text = "Ingrese el consumo mensual: ";
-                        customers[x][y][z] = Validations.valDouble(text);
+                        customers[x][y][z] = Validations.valDouble("Ingrese el consumo mensual: ");
                     }
                 }
             }
@@ -84,7 +79,7 @@ public class Process {
         }
     }
 
-     public static void calculatePrice(double[][][] customers, double [] cost) {
+    public static void calculatePrice(double[][][] customers, double [] cost) {
 
         double sum;
         if (customers.length != 0 && cost.length != 0) {
@@ -99,4 +94,90 @@ public class Process {
         }
     }
 
+    public static void hig_LowConsume(double[]cost, String[] customersnames, String[] consume) {
+
+        int max = 0 ; int low = 0;
+
+        if (cost.length != 0 && customersnames.length != 0 && consume.length !=0) {
+            for(int x = 0; x<cost.length; x++){
+                if (cost[x] > cost[max]) {
+                    max = x;
+                }
+
+                if (cost[x] < cost[low]) {
+                    low = x;
+                }
+            }
+            consume [0] = customersnames[max];
+            consume [1] = customersnames[low];
+        }
+    }
+
+    public static void calculaterMunipality(double [] price, double [] finalPrice, String [] customernames){
+        int municipality =0;
+        String texts = "";
+        System.out.println("Seleccione un municipio de residencia:\n1.juanjosemora\n2.puertocabello\n3.sandiego\n4.valencia\n5.losguayos");
+
+        if (price.length != 0 && finalPrice.length != 0) {
+            for (int i = 0; i < price.length; i++) {
+                texts = "Ingrese el numero del municipio del cliente ["+(i+1)+"]["+customernames[i]+"]: ";
+                municipality = Validations.valMaxvalues(texts,6);
+                if (municipality == 1){
+                    finalPrice [i] = price[i] + price[i] * 0.07;
+                }
+                if (municipality == 2){
+                    finalPrice [i] = price[i] + price[i] * 0.05;
+                }
+                if (municipality == 3){
+                    finalPrice [i] = price[i] + price[i] * 0.08;
+                }
+                if (municipality == 4){
+                    finalPrice [i] = price[i] + price[i] * 0.1;
+                }
+                if (municipality == 5){
+                    finalPrice [i] = price[i] + price[i] * 0.03;
+                }
+            }
+        }
+    }
+
+    public static void showBill(double[][] average, double [] finalPrice, String []consume, String [] customersNames,String router){
+        String texts = "";
+
+        Validations.useArchive("                                | Inicio del Programa                |", router, true);
+        Validations.useArchive("", router, true);
+        Validations.useArchive(String.format("%-5s | %-25s | %-18s | %-15s", "Casa", "Nombre del propietario", "Promedio del", "Costo total"), router, false);
+        Validations.useArchive(String.format("%-5s | %-25s | %-18s | %-15s", "    ", " de la casa             ", " consumo de kw  ", " de kw          "), router, true);
+        Validations.useArchive("", router, true);
+        if (average != null && finalPrice != null && consume != null && customersNames!= null) {
+
+            for (int i = 0; i < customersNames.length; i++) {
+
+                texts = String.format("%-5d | %-25s | %-15.2f kw/mes | %-12.2f$", (i + 1), customersNames[i], average[i][0], finalPrice[i]);
+                Validations.useArchive(texts, router, true);
+            }
+            Validations.useArchive("", router, true);
+            Validations.useArchive("-------------------------------------------------------------------------------", router, true);
+            Validations.useArchive("", router, true);
+            Validations.useArchive("Cliente con mas consumido de kw: " + consume[0], router, true);
+            Validations.useArchive("Cliente con menos consumido de kw : " + consume[1], router, true);
+        }
+    }
+
+    public static void showAverage(double[][]average, double [] finalprice, String [] consume){
+
+        if (average != null) {
+            for (int i = 0; i < average.length; i++) {
+                for (int j = 0; j < average[i].length; j++) {
+                    System.out.println(average[i][j]);
+                }
+                System.out.println(finalprice[i]);
+                System.out.println();
+            }
+            System.out.println();
+            for (int i = 0; i < consume.length; i++) {
+                System.out.println(consume[i]);
+            }
+        }
+    }
 }
--- arr/Validations.java
+++ arr/Validations.java
@@ -1,5 +1,9 @@
 package arr;
-
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.time.LocalDateTime;
 import java.util.InputMismatchException;
 import java.util.Scanner;
 
@@ -44,6 +48,28 @@ public class Validations {
         }
     }
 
+
+    public static int valMaxvalues(String text, int maxValues) {
+        int value = 0;
+        Scanner scanner = new Scanner(System.in);
+        while (true) {
+            try {
+                System.out.print(text);
+                value = scanner.nextInt();
+                if (value >= maxValues) {
+                    System.out.println("Error: El valor debe ser menor a " + maxValues);
+                } else {
+                    return value;
+                }
+            } catch (InputMismatchException e) {
+                System.out.println("Error: [No se aceptan caracteres]");
+                scanner.nextLine();
+            }
+        }
+    }
+
+    
+
   public static boolean valString(String text)
     {
         String limit = "0123456789!#$%/()=?¡¨*[]_";
@@ -73,4 +99,53 @@ public class Validations {
         }
     }
 
+    public static String utilDirectory(String router) {
+
+        if (router.trim().isEmpty()) {
+            System.out.println("Manage-Error: Ruta no existe.");
+            return null;
+        }
+
+        File directories = new File(router);
+        if (!directories.exists()) {
+            System.out.println("Manage-Error: El directorio a guardar no existe. ");
+            return null;
+        }
+        return router;
+
+    }
+
+    public static String nameArchiveGenerate() {
+        LocalDateTime actualDateTime = LocalDateTime.now();
+        int rand = (int) (Math.random() * 100) + 1;
+        String name = "resultaExam";
+        
+        String safeDateTime = actualDateTime.toString()
+            .replace(":", "_")
+            .replace("T", "_")
+            .replace(".", "_");
+        return name + "_" + safeDateTime + "_Serial" + rand;
+    }
+
+    public static void useArchive(String content, String route, boolean bool)
+    {
+        if (route.trim().isEmpty()) {
+            System.out.println("Manage-Error: Ruta no existe.");
+            return;
+        }
+
+        try (BufferedWriter addArchive = new BufferedWriter(new FileWriter(route, true))) { 
+            if (content == null || content.trim().isEmpty()) {
+                throw new IllegalArgumentException(" EL nombre del archivo es requerido. ");
+            }
+
+            addArchive.write(content);  
+            if (bool){
+                addArchive.newLine(); 
+            }
+        } catch (IOException e) {
+            System.out.println("- Error al escribir en el archivo: " + e.getMessage());
+            return;
+        }
+    }
 }
